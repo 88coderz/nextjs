9e44f75081a4652d0549afab3f24c7be454b7df7
running water never stales <91630063+letzwrk@users.noreply.github.com>
2023-11-06 17:10:06 -0600

    adding services and heading

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
 
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Site also under construction but still serving Travis County !&nbsp;
+          Serving Travis County while site under construction&nbsp;
           <code className="font-mono font-bold">(512)809-0129</code>
         </p>
 
@@ -15,31 +15,38 @@ export default function Home() {
 
       </div>
 
-      <section className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+      <section className="relative place-items-center before:absolute before:h-[200px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
 
-      
+        <h1 className="text-5xl" >Handyman Extraordinaire</h1>       
+        <h2 className="text-2lg">services ranging from :</h2>
+
+        <ul>
+          <li>Ceiling Fans/Lights</li>
+          <li>Switches and Outlets</li>          
+        </ul>  
+        
+        <ul>
+          <li>Sinks & Faucets</li>
+          <li>Floats and Valves</li>
+          <li>Toilets and Wax Rings</li>
+        </ul>
  
       </section>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        >
+        <a className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"      >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Forum{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+              -&gt;  </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            List what needs to be done so contractors can make bids!
+            List what you need done and contractors can make bids!
           </p>
         </a>
 
         <a
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        > 
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"     > 
           <h2 className={`mb-3 text-2xl font-semibold`}>
             AI Feedback{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
@@ -47,27 +54,23 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Scrape the internet for some suggestions to your project. Just always ask a professional to verify the information while AI is training!
+            Scrape the internet for suggestions to your project. Just always ask a professional to verify the information while AI is training!
           </p>
         </a>
 
         <a
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        >
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"      >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Barter{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+              -&gt;       </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Lets make sure only the best deals are made. List your chat and projects publically so you can have the market interact and make sure you are getting a great deal. 
-          </p>
+            Lets make sure only the best deals are made. List your chat and projects publically so you can have the market interact and make sure you are getting a great deal.         </p>
         </a>
 
         <a
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        >
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"  >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Blog & Community{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
