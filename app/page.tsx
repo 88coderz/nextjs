4b8c4732d6658d604f018b7523fc8d23aad2702c
running water never stales <91630063+letzwrk@users.noreply.github.com>
2023-11-08 23:18:29 -0600

    patch

--- app/page.tsx
+++ app/page.tsx
@@ -102,11 +102,11 @@ export default function Home() {
         </article>
 
       </section>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-
+      
       <h6 className="text-5xl py-8 sm:py-8" >Whats Being Built</h6>  
       <h2 className="text-2lg">https://www.github.com/88coderz/nextjs</h2>
+
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
 
         <a className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"      >
           <h2 className={`mb-3 text-2xl font-semibold`}>
