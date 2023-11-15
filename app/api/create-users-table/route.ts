213b00b9eeb1e1808c1a2e9eb92b90f8d5540295
running water never stales <91630063+letzwrk@users.noreply.github.com>
2023-11-14 23:24:05 -0600

    privacy setting change

--- app/api/create-users-table/route.ts
+++ app/api/create-users-table/route.ts
@@ -3,12 +3,12 @@ import { NextResponse } from 'next/server';
  
 export async function GET(request: Request) {
   try {
-    const result = await sql`CREATE TABLE Users ( Name varchar(255), Contact_info varchar(255) );`;
-    
-    
+    const result = await sql`CREATE TABLE Users ( Name varchar(255), Contact_info varchar( 255 ) );`;    
       return NextResponse.json({ result }, { status: 200 });
-  } catch (error) {
-    
+
+  } catch ( error ) {    
     return NextResponse.json({ error }, { status: 500 });
+
   }
+  
 }
