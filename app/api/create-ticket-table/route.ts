237be733346db18bcef5f351d4a66aa76df964e0
running water never stales <91630063+letzwrk@users.noreply.github.com>
2023-11-24 03:08:21 -0600

    vercel analytics

--- app/api/create-ticket-table/route.ts
+++ app/api/create-ticket-table/route.ts
@@ -3,12 +3,9 @@ import { NextResponse } from 'next/server';
  
 export async function GET(request: Request) {
   try {
-    const result = await sql`CREATE TABLE Ticket ( Originator-id varchar(255), ticket-num varchar( 255 ) );`;    
-      return NextResponse.json({ result }, { status: 200 });
-
-  } catch ( error ) {    
-    return NextResponse.json({ error }, { status: 500 });
-
+    const result = await sql`CREATE TABLE Ticket (client-id varchar(255), ticket-num varchar(255));`;    
+      return NextResponse.json({result}, {status: 200});
+  } catch (error) {    
+    return NextResponse.json({error}, {status: 500});
   }
-
 };
