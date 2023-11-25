93394a0c037f197117da4d0f34046f4d3d7145ca
running water never stales <91630063+letzwrk@users.noreply.github.com>
2023-11-25 16:53:39 -0600

    making phone number a link

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
 
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Serving our  greater Abilene area &nbsp;
-          <code className="font-mono font-bold">(512)809-0129</code>
+          <a href="tel:1-512-809-0129" className="font-mono font-bold">(512)809-0129</a>
         </p>
 
         <div className="fixed bottom-0 left-0 flex h-8 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none" >
@@ -17,7 +17,7 @@ export default function Home() {
 
       <section className="relative place-items-center before:absolute before:h-[200px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
 
-      <h1 className="text-5xl py-8 sm:py-8" >Handyman Extraordinaire</h1>  
+      <h1 className="text-5xl py-8 sm:py-8"> Handyman Extraordinaire</h1>  
       <h2 className="text-2lg">Services ranging from but not limited to:</h2>
 
         <article className="grid grid-cols-3 gap-7 mx-auto max-w-xxl 
