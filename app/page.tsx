6de73c7e4289ced6025bfb3143d261c3ae218a41
running water never stales <91630063+letzwrk@users.noreply.github.com>
2023-12-16 16:53:30 -0600

    list icon

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,29 @@
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
 
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Serving our  greater Abilene area &nbsp;
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit ">
+          Serving our greater Abilene area &nbsp; 
           <a href="tel:1-512-809-0129" className="font-mono font-bold">(512)809-0129</a>
+
+          <div className="fixed top-0 right-0">
+            <Image 
+              src="/list.svg"
+              className="dark:invert"
+              width={50}
+              height={50}
+              alt="list-icon"
+            /> 
+          </div>
+ 
+
         </p>
 
-        <div className="fixed bottom-0 left-0 flex h-8 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none" >
+
+        <div className="fixed bottom-0 left-0 flex h-8 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black" >
             questions about your project are free !
         </div>
 
@@ -111,7 +125,7 @@ export default function Home() {
 
         <a className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"      >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Forum{' '}
+            Work List & Forum{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;  </span>
           </h2>
