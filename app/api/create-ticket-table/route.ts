304c2a6dbae053953e9dde99924502ccfca53521
running water never stales <91630063+letzwrk@users.noreply.github.com>
2024-01-24 08:26:25 -0600

    adding Request types

--- app/api/create-ticket-table/route.ts
+++ app/api/create-ticket-table/route.ts
@@ -1,11 +1,35 @@
 import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
+
+interface Request {
+
+   ticket_num: number;
+   created_at: string;
+   user: string;
+   date: number; 
+   category: string;
+   
+   completed: boolean;
+}
  
 export async function GET(request: Request) {
+
   try {
-    const result = await sql`CREATE TABLE Ticket (client-id varchar(255), ticket-num varchar(255));`;    
-      return NextResponse.json({result}, {status: 200});
-  } catch (error) {    
-    return NextResponse.json({error}, {status: 500});
-  }
+
+     const    result = await    sql `CREATE TABLE Ticket ( client-id varchar(255) , ticket-num varchar(255) ); ` ; 
+
+      return  NextResponse.json( { result } ,  { status: 200 }  ) ;
+
+  }   catch (error) {       
+   
+     return NextResponse.json( {error}, {status: 500} ); 
+   
+   }  finally ( ) {
+
+      return()
+
+   }
+   
 };
+
+
