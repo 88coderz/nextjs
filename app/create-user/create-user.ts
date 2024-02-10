d5e6b34994f1681f573ac565b79c842f65bd36b9
running water never stales <91630063+letzwrk@users.noreply.github.com>
2024-02-09 22:57:06 -0600

    export and return <NewUser/>

--- app/create-user/create-user.ts
+++ app/create-user/create-user.ts
@@ -1,31 +1,39 @@
 // https://www.typescriptlang.org/docs/handbook/2/classes.html
 
+export default function CreateNewUser():any {
+
     class NewUser {         
-            location: string;
-            auth: boolean;
-            associatedTickets: boolean;
-            avitar: boolean;
-            userName: string;
-    
+        location: string;
+        auth: boolean;
+        associatedTickets: boolean;
+        avitar: boolean;
+        userName: string;
+
 //  super(): any ;  <<<< when is super() applied ????
 
-        constructor (
-            location = 'Austin, Texas',
-            auth = false,
-            associatedTickets = false,
-            avitar = false,
-            userName = 'New User',
+    constructor (
+        location = 'Austin, Texas',
+        auth = false,
+        associatedTickets = false,
+        avitar = false,
+        userName = 'New User',
 
-        ) {
-            this.location = location,
-            this.auth = auth,
-            this.associatedTickets = associatedTickets,
-            this.avitar = avitar ,
-            this.userName = userName ;         
-            
+    ) {
+        this.location = location,
+        this.auth = auth,
+        this.associatedTickets = associatedTickets,
+        this.avitar = avitar ,
+        this.userName = userName ;         
+        
         }      
     }
+
     
+
+    return  NewUser
+
+
+}
 
 
 
