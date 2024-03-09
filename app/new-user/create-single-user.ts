f5033c3a6411a60d8985bb84ef3ff2af53ea16ae
running water never stales <91630063+letzwrk@users.noreply.github.com>
2024-03-08 19:44:17 -0600

    reactifying <input/>

--- app/new-user/create-single-user.ts
+++ app/new-user/create-single-user.ts
@@ -1,17 +1,12 @@
-
 // https://www.typescriptlang.org/docs/handbook/2/classes.html
-
-    class NewSingleUser {   
-
+class NewSingleUser {   
         location: string;
         auth: boolean;
         associatedTickets: boolean;
         avitar: boolean;
         email: string ;
         userName: string;
-
 //  super(): any ;  <<<< when is super() applied ????
-
     constructor (
         location = 'Austin, Texas',
         auth = false,
@@ -19,9 +14,7 @@
         avitar = false,
         email = 'insert@email.com',
         userName = 'New User',
-
     ) {
-
         this.location = location,
         this.auth = auth,
         this.associatedTickets = associatedTickets,
@@ -29,13 +22,5 @@
         this.email = email , 
         this.userName = userName ;    
         }      
+}    
 
-    }    
-
-// https://nextjs.org/docs/app/building-your-application/routing
-
-// https://nextjs.org/docs/app/api-reference/file-conventions/route
-
-// https://nextjs.org/docs/app/api-reference/functions/next-request
-
-// https://vercel.com/docs/storage/vercel-postgres/sdk#createclient
--- app/new-user/handler.ts
+++ app/new-user/handler.ts
@@ -4,21 +4,16 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function POST (
     request:   NextRequest  | Request  , 
     response:  NextResponse | Response ,
-  ) {    
-  try { const newUser = await sql ` CREATE TABLE User ( 
+  ) { try { const newUser = await sql ` CREATE TABLE User ( 
       name             VARCHAR ( 255 ) NOT NULL , 
       email            VARCHAR ( 255 ) NOT NULL ,
       phone-num        VARCHAR ( 255 ) NOT NULL ,
       client_id        INT NOT NULL PRIMARY KEY ,
-      is_new_user      BOOLEAN NOT NULL DEFAULT TRUE ,      
-      );`;    
-
-      return NextResponse.json (
-        { newUser }, { status: 200 }  );
+      is_new_user      BOOLEAN NOT NULL DEFAULT TRUE );`;       
+return NextResponse.json( { newUser }, { status: 200 } );
   } catch ( error ) {    
-    return NextResponse.json ( { error }, { status: 500 } );
-  }
-}; // defining datatypes of nextREQ & nextRES 
+    return NextResponse.json( { error }, { status: 500 } );
+  } }; // defining datatypes of nextREQ & nextRES 
 
 interface Request {
   current_user:  boolean;
@@ -27,7 +22,6 @@ interface Request {
   client_id:     string;
   location:      string;
 }
-//difine nextResponse too ? 
 interface Response {
   current_user:  boolean;
   created_at:    string;
@@ -37,30 +31,20 @@ interface Response {
 } // https://vercel.com/docs/storage/vercel-postgres/sdk#createclient
 import { createClient } from '@vercel/postgres' ; 
 import '.env.local' ;
-// Individual clients can be created, connected, and disconnected for each query. This method is less efficient than using db and should only be used when a single client is required.
 
 const newUser = createClient( /* { connectionString: process.env.POSTGRES_URL } */ ) ;
 
 export default async function handler(
     req: NextRequest , res: NextResponse ,
     userExists: boolean , 
-    ) { try { 
-      
-      await newUser.connect();
-    
+    ) { try { await newUser.connect();    
     switch (req.method) {
-      case 'GET':
-        // verification user doesn't exist 
+      case 'GET':  // verification that user doesn't exist 
         const userName = await newUser.query('SELECT 1 FROM users WHERE username = $1', [userExists])
         break;
       case 'POST':
         // new user info
         if (!userExists) { newUser };
-
-
         break;
-      default:
-        
-    }     
-  } finally { newUser.end(); } 
-}
+      default:        
+    } } finally { newUser.end(); } }
